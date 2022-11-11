2ac8abfafe64e4baf31fd6420d74df167e19cbdd
Lina Soler <lipasofra.ph@gmail.com>
2022-11-10 20:07:21 -0500

    exercise 3.19 name validation

--- index.js
+++ index.js
@@ -99,15 +99,9 @@ app.post('/api/persons', (request, response, next) => {
 
 app.put('/api/persons/:id', (request, response, next) => {
 
-  const body = request.body
+  const {name, number} = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-
-  Person.findByIdAndUpdate(request.params.id, person, {new: true}).then(
+  Person.findByIdAndUpdate(request.params.id, {name, number}, {new: true, runValidators: true, context: 'query'}).then(
     updatedPerson => response.json(updatedPerson)
   ).catch(
     error => next(error)
@@ -120,6 +114,8 @@ const errorHandler = (error, request, response, next) => {
   console.log(error.message)
   if (error.name === 'CastError'){
     return response.status(400).send({error: "malformatted id"})
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
